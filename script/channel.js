c24b598451cc79a087780f196e0ff10d610585d6
Your Name <you@example.com>
2022-02-28 08:32:53 +0200

    Clean up channel.js a bit

--- script/channel.js
+++ script/channel.js
@@ -1,18 +1,9 @@
-let lbryUrl = document.URL.match(new RegExp(/lbry:\/\/.*/))[0];
-lbryUrl = decodeURIComponent(lbryUrl);
-let channel = lbryUrl.match(new RegExp(/@.*/))[0];
-let server = "http://localhost:5279";
-let channel_claim = {};
-let page = 1;
 
+function setInfo(claim) {
+	console.log(claim);
 
-function setInfo(obj) {
-	obj = obj.result[lbryUrl];
-	channel_claim = obj;
-	console.log(obj);
-
-	let profile_img = (obj.value.thumbnail ? obj.value.thumbnail.url : "");
-	let title = (obj.value.title ? obj.value.title : obj.name);
+	let profile_img = (claim.value.thumbnail ? claim.value.thumbnail.url : "");
+	let title = (claim.value.title ? claim.value.title : claim.name);
 
 	let profile_img_elem = document.querySelector("#channel_thumbnail");
 	profile_img_elem.src = profile_img;
@@ -22,24 +13,22 @@ function setInfo(obj) {
 	
 }
 
-let xhr2 = new XMLHttpRequest();
-xhr2.open("POST", server, true);
-xhr2.onreadystatechange = function() {
-	if (xhr2.readyState === 4 && xhr2.status === 200) {
-		search_params = {
-			"channel": channel,
-			"order_by": "release_time",
-			"page": page++
-		}
-		let obj = JSON.parse(xhr2.response);
-		setInfo(obj);
-		sendSearchParams(search_params, obj.result[lbryUrl])
-		
-	}
-};
-xhr2.send(JSON.stringify({
-	method: "resolve",
-	params: {
-		"urls": lbryUrl}
-}));
+function channel_main() {
+	let lbryUrl = document.URL.match(new RegExp(/lbry:\/\/.*/))[0];
+	lbryUrl = decodeURIComponent(lbryUrl);
+	let channel = lbryUrl.match(new RegExp(/@.*/))[0];
+
+	doACall("resolve", {urls: lbryUrl}, (response) => {
+			search_params = {
+				channel: channel,
+				order_by: "release_time",
+			};
+			let channel_claim = response.result[lbryUrl];
+			setInfo(channel_claim);
+			sendSearchParams(search_params, channel_claim);
+
+	});
+}
+
+channel_main();
 
