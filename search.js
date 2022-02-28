a95a7ae322746b6d1daa7bb16c0cd1906543b086
Your Name <you@example.com>
2022-02-28 05:25:08 +0200

    Clean up search.js a bit and minor updates

--- search.js
+++ search.js
@@ -1,27 +1,3 @@
-let url = new URL(document.URL);
-let search_term = url.searchParams.get("search_term");
-
-// If searhing for URL
-if (search_term.match(new RegExp("^lbry://.*"))) {
-	let page = getClaimTypeFromUrl(search_term);
-	window.location.replace(page+"?url="+search_term);
-}
-
-let lbry_url = "lbry://"+search_term.replace(/\s|"/g, "");
-let server = "http://localhost:5279";
-let page = 1;
-
-let search_params = {
-	text: search_term,
-	claim_type: "stream",
-	has_source: true,
-	order_by: "trending_mixed",
-	page: page,
-	no_totals: true
-}
-
-sendSearchParams(search_params);
-
 function getClaimTypeFromUrl(lbryUrl) {
 		if (lbryUrl.match(new RegExp(".*@.*")) &&
 				!lbryUrl.match(new RegExp(".*//.*/.*")))
@@ -31,27 +7,27 @@ function getClaimTypeFromUrl(lbryUrl) {
 }
 
 
-function createMostSupported(obj) {
-	obj = obj.result[lbry_url];
-	if (obj.value_type === "repost")
-		obj = obj.reposted_claim;
-	if (txidIsFiltered(obj.txid) || (obj.signing_channel ? txidIsFiltered(obj.signing_channel.txid) : false)) {
+function createMostSupported(response, lbry_url) {
+	let claim = response.result[lbry_url];
+	if (claim.value_type === "repost")
+		claim = claim.reposted_claim;
+	if (txidIsFiltered(claim.txid) || (claim.signing_channel ? txidIsFiltered(claim.signing_channel.txid) : false)) {
 		//return;
 	}
 
-	let data = obj.value;
+	let data = claim.value;
 	let community_choice_div = document.querySelector("#community_choice_div");
 	a = document.querySelector("#community_choice_a");
-	a.href = getClaimTypeFromUrl(obj.permanent_url)+"?url="+obj.permanent_url;
+	a.href = getClaimTypeFromUrl(claim.permanent_url)+"?url="+claim.permanent_url;
 	let thumbnail_elem = document.createElement("img");
 	let title_elem = document.createElement("h4");
 	let release_time_elem = document.createElement("p");
 
-	if (obj.value_type === "channel")
+	if (claim.value_type === "channel")
 		thumbnail_elem.classList.add("channel");
 	thumbnail_elem.src = (data.thumbnail ? data.thumbnail.url : "");
-	title_elem.innerHTML = (data.title ? data.title : obj.name);
-	release_time_elem.innerHTML = (data.release_time ? data.release_time : obj.meta.creation_timestamp);
+	title_elem.innerHTML = (data.title ? data.title : claim.name);
+	release_time_elem.innerHTML = (data.release_time ? data.release_time : claim.meta.creation_timestamp);
 
 	community_choice_div.append(thumbnail_elem);
 	community_choice_div.append(title_elem);
@@ -68,7 +44,7 @@ function createMostSupported(obj) {
 			json_viewer = new JSONViewer();
 			json_div.appendChild(json_viewer.getContainer());
 			main_div.insertBefore(json_div, hr_div.nextSibling);
-			json_viewer.showJSON(obj, -1, 1);
+			json_viewer.showJSON(claim, -1, 1);
 		} else {
 			json_viewer.remove();
 		}
@@ -77,8 +53,32 @@ function createMostSupported(obj) {
 
 }
 
-doACall("resolve", {urls: lbry_url}, (response) =>{
-		createMostSupported(response);
-});
 
+function search_main() {
+	let url = new URL(document.URL);
+	let search_term = url.searchParams.get("search_term");
+	// If searhing for URL naviagte to page
+	if (search_term.match(new RegExp("^lbry://.*"))) {
+		let page = getClaimTypeFromUrl(search_term);
+		window.location.replace(page+"?url="+search_term);
+	}
+
+	let lbry_url = "lbry://"+search_term.replace(/\s|"/g, "");
+	doACall("resolve", {urls: lbry_url}, (response) =>{
+			createMostSupported(response, lbry_url);
+	});
+
+	let search_params = {
+		text: search_term,
+		claim_type: "stream",
+		has_source: true,
+		order_by: "trending_score",
+		no_totals: true
+	}
+
+
+	sendSearchParams(search_params);
+}
+
+search_main();
 
